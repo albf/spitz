85859b3f330207f04d9dad2be63be710dd97e907
Alexandre <albf.unicamp@gmail.com>
2014-10-12 19:20:41 -0300

    script.sh added

---
+++ examples/script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+i=0
+while read host; do
+  case $i in 
+  0)
+  jmip=$(grep $host /etc/hosts | cut -d'	' -f1)
+  ssh $host "LD_LIBRARY_PATH=\$HOME/spitz/spitz/examples SPITS_LOG_LEVEL=2 \$HOME/spitz/spitz/examples/spitz 0 $jmip prime.so 30" &
+  ;;
+  1)
+  ssh $host "LD_LIBRARY_PATH=\$HOME/spitz/spitz/examples SPITS_LOG_LEVEL=2 \$HOME/spitz/spitz/examples/spitz 1 $jmip prime.so 30" &
+  ;;
+  *)
+  ssh $host "LD_LIBRARY_PATH=\$HOME/spitz/spitz/examples SPITS_LOG_LEVEL=2 \$HOME/spitz/spitz/examples/spitz 2 $jmip prime.so 30" &
+  ;;
+  esac
+  i=`expr $i + 1`
+done < $PBS_NODEFILE
+wait
