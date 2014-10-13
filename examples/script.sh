61dd56c017a313b66a1bdad8c8525dd295fa13a0
Alexandre <albf.unicamp@gmail.com>
2014-10-12 22:09:34 -0300

    script.sh update

--- examples/script.sh
+++ examples/script.sh
@@ -4,13 +4,13 @@ while read host; do
   case $i in 
   0)
   jmip=$(grep $host /etc/hosts | cut -d'	' -f1)
-  ssh $host "LD_LIBRARY_PATH=\$HOME/spitz/spitz/examples SPITS_LOG_LEVEL=2 \$HOME/spitz/spitz/examples/spitz 0 $jmip prime.so 30" &
+  ssh $host "LD_LIBRARY_PATH=\$HOME/codes/spitz/examples SPITS_LOG_LEVEL=2 \$HOME/codes/spitz/examples/spitz 0 $jmip prime.so 30" &
   ;;
   1)
-  ssh $host "LD_LIBRARY_PATH=\$HOME/spitz/spitz/examples SPITS_LOG_LEVEL=2 \$HOME/spitz/spitz/examples/spitz 1 $jmip prime.so 30" &
+  ssh $host "LD_LIBRARY_PATH=\$HOME/codes/spitz/examples SPITS_LOG_LEVEL=2 \$HOME/codes/spitz/examples/spitz 1 $jmip prime.so 30" &
   ;;
   *)
-  ssh $host "LD_LIBRARY_PATH=\$HOME/spitz/spitz/examples SPITS_LOG_LEVEL=2 \$HOME/spitz/spitz/examples/spitz 2 $jmip prime.so 30" &
+  ssh $host "LD_LIBRARY_PATH=\$HOME/codes/spitz/examples SPITS_LOG_LEVEL=2 \$HOME/codes/spitz/examples/spitz 2 $jmip prime.so 30" &
   ;;
   esac
   i=`expr $i + 1`
