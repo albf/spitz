b8420d9501ccf09331a6372807b15abf0dd3d5dd
albf <albf.unicamp@gmail.com>
2015-01-28 01:28:11 -0200

    list fix2

--- spitz/list.c
+++ spitz/list.c
@@ -192,6 +192,14 @@ struct LIST_data * LIST_remove_id(struct LIST_data * data_pointer, int id) {
    
     // First check for the head.
     if(data_pointer->list_pointer->id == id) {
+        // If it is here and there is any holes, check again, might break.
+        if(ptr->next != NULL) {
+            // Fix the holes number in the head.
+            if((ptr->next->id)!=((ptr->id)-1) ) {
+                data_pointer->holes = (data_pointer->holes) - ((ptr->id)-1-(ptr->next->id));
+            }
+        }
+
         free(data_pointer->list_pointer->address);
     	ptr = data_pointer->list_pointer->next;
         free(data_pointer->list_pointer);
