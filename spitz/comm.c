5870eec781fb7129d75a4a6caacf4d706894e5be
albf <albf.unicamp@gmail.com>
2015-05-13 03:58:54 -0300

    Major comm fix

--- spitz/comm.c
+++ spitz/comm.c
@@ -75,6 +75,8 @@ int COMM_send_message(struct byte_array *ba, int type, int dest_socket) {
       ba = &_ba;
     }
 
+    debug("Sending message size: %d\n", ba->len);
+
     return_value = COMM_send_int(dest_socket, type);
     if(return_value < 0) {
         error("Problem sending message type");
@@ -116,6 +118,8 @@ int COMM_read_message(struct byte_array *ba, enum message_type *type, int rcv_so
         *type = MSG_EMPTY;
     }
 
+    debug("Reading message size: %d\n", ba->len);
+
     return read_return;
 }
 
@@ -125,6 +129,7 @@ int COMM_send_bytes(int sock, void * bytes, int size) {
     int return_value;
     
     sprintf(size_c, "%d", size);
+    debug("SENDBYTES_ MSG_SIZE: %d\n", size);
     strcat(size_c, "|\n");
     return_value = (int) send(sock, size_c, (strlen(size_c)-1), MSG_NOSIGNAL); // don't send '\n'
 
@@ -163,13 +168,14 @@ int COMM_read_bytes(int sock, int * size, struct byte_array * ba, int null_termi
             message_size[offset]=received_char;
         }
         else {
-            message_size[offset]='\n';
+            message_size[offset]='\0';
         }
 
         offset++;
     }
 
     msg_size = atoi(message_size);
+    debug("READBYTES _ MSG_SIZE: %d\n", msg_size);
     offset = 0;
     
     if(null_terminator > 0) {           // Check if should reserve space for null terminator. 
@@ -184,9 +190,13 @@ int COMM_read_bytes(int sock, int * size, struct byte_array * ba, int null_termi
     	* size = msg_size;    
     }
         
+    ba->len = 0;
     while(offset < msg_size) {		// if zero, doesn't come in
         total_rcv = read(sock, (ba->ptr+offset), (msg_size-offset));
-        ba->len += total_rcv;
+        ba->len += ((size_t)total_rcv);
+        debug("READBYTES_DURING_DIF : %d\n", (msg_size-offset));
+        debug("READBYTES_DURING_RCV : %d\n", total_rcv);
+        debug("READBYTES_DURING_TOT : %d\n", (int)ba->len);
 
         if(total_rcv <= 0) {            // check if received zero bytes or an error.
             if(size != NULL) {
